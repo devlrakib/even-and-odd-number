f0427af5d0e54bcacfa8f710559c4dd07c1c8005
devlrakib <devrakib5@gmail.com>
2024-03-14 19:58:46 +0600

    first commit

---
+++ even.js
@@ -0,0 +1,6 @@
+var priceMoney = 0;
+while (priceMoney < 6){
+    console.log(priceMoney);
+   // priceMoney++;
+   priceMoney = priceMoney + 2;
+}
---
+++ loop-variable-sequence.js
@@ -0,0 +1,8 @@
+/*
+Loop Variable Writting Rules:
+01. Loop Variable 
+02. Condition Inside While 
+03. Loop Body
+04. Change The Loop Variable 
+
+*/
